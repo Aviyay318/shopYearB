e490f5ca5a31d6a750f11b64631ba7d61fd36609
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2026-01-20 20:33:00 +0200

    add course

--- src/main/java/org/example/shopyearb/Utils/Constant.java
+++ src/main/java/org/example/shopyearb/Utils/Constant.java
@@ -4,4 +4,5 @@ public class Constant {
     public static final int ERROR_REGISTER = 1001;
     public static final int ERROR_LOGIN = 1003;
     public static final int ERROR_ADD_PRODUCT = 1002;
+    public static final int COURSE_EXIST = 1004;
 }
